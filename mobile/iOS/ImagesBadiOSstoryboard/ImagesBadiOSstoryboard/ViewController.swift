07870eca4c13b71c0d470fa13b64181de2e094bc
Paul J. Adam <paul@pauljadam.com>
2022-05-23 21:45:24 -0500

    mobile stuff

---
+++ mobile/iOS/ImagesBadiOSstoryboard/ImagesBadiOSstoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagesBadiOSstoryboard
+//
+//  Created by Paul Adam on 5/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
